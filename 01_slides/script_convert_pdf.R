c5f587354d8eb98d6c32a11354bb486fe038e421
mauriciovancine <mauricio.vancine@gmail.com>
2022-04-05 17:33:21 -0300

    1.0.0

---
+++ 01_slides/script_convert_pdf.R
@@ -0,0 +1,18 @@
+#' ---
+#' title: convert rmarkdown into pdf
+#' author: mauricio vancine
+#' date: 2021-10-20
+#' ---
+
+# packages
+library(pagedown)
+library(xaringan)
+library(tidyverse)
+library(R.utils)
+
+# convert to pdf
+setwd("01_slides/")
+pagedown::chrome_print("slides_workshop_qgis.Rmd", timeout = 1e5)
+
+
+# end ---------------------------------------------------------------------
